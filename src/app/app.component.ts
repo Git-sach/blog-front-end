06c35555f7a51eb054efbd92f0699fc4059dc39b
Adrien Cheynet <gitcha@outlook.fr>
2024-02-24 11:46:55 +0100

    POC ngx-editor + POC Table Of Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/containers/home/home.component';
+import { PostViewComponent } from './features/containers/post-view/post-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, PostViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
